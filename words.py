0f99f31b0486dcc6f97456776175a92826f1d29c
lizirukh <lizirukh@gmail.com>
2023-03-15 18:51:33 +0400

    first commit

---
+++ words.py
@@ -0,0 +1,16 @@
+n = int(input())
+dict = {
+
+}
+
+while n>0:
+    word = input()
+    if word not in dict:
+        dict[word] = 1
+        n-=1
+    else:
+        dict[word] += 1
+        n-=1
+print(len(dict))
+dictList = list(dict.values())
+print(dictList)          
