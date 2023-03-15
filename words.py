e2881d00789df47714c5ede13fb596ae90c56c11
lizirukh <lizirukh@gmail.com>
2023-03-15 19:05:53 +0400

    moving n decrement

--- words.py
+++ words.py
@@ -7,10 +7,10 @@ while n>0:
     word = input()
     if word not in dict:
         dict[word] = 1
-        n-=1
+        # n-=1
     else:
         dict[word] += 1
-        n-=1
+    n-=1
 print(len(dict))
 dictList = list(dict.values())
 print(dictList)          
